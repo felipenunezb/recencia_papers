47c687ff731ba16b41a5f0612344441e6a0f7fe2
felipenunezb <f.nunezb@gmail.com>
2021-09-04 21:58:39 -0400

    do not require model

--- prepare_data.py
+++ prepare_data.py
@@ -36,24 +36,6 @@ def parse_args():
         action="store_true",
         help="If passed, pad all samples to `max_length`. Otherwise, dynamic padding is used.",
     )
-    parser.add_argument(
-        "--model_name_or_path",
-        type=str,
-        help="Path to pretrained model or model identifier from huggingface.co/models.",
-        required=True,
-    )
-    parser.add_argument(
-        "--config_name",
-        type=str,
-        default=None,
-        help="Pretrained config name or path if not the same as model_name",
-    )
-    parser.add_argument(
-        "--tokenizer_name",
-        type=str,
-        default=None,
-        help="Pretrained tokenizer name or path if not the same as model_name",
-    )
     parser.add_argument("--output_dir", type=str, default=None, help="Where to store the final embeddings.")
     parser.add_argument("--seed", type=int, default=0, help="A seed for reproducible training.")
     parser.add_argument(
